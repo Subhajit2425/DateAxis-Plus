26fb4a801022e47048126b638c6723d9d9b14f64
Subhajit Paramanik <paramaniksubhajit2007@gmail.com>
2025-12-09 16:08:58 +0530

    Addition

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import Header from './MyComponents/Header';
 import Todos from './MyComponents/Todos';
 import Footer from './MyComponents/Footer';
-import AddTodo from "./MyComponents/AddTodo";
-import React, { useState } from 'react';
+import {AddTodo} from "./MyComponents/AddTodo";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos")===null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
   const onDelete = (todo) =>{
     console.log("I am ondelete of todo", todo);
     // Deleting this way in react does not work
@@ -14,31 +20,38 @@ function App() {
 
     setTodos(todos.filter((e)=>{
       return e!== todo;
-    }))
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
-  const [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: "You need to go to the market",
-      desc: "You need to go to market to get this job done"
-    },
-    {
-      sno: 2,
-      title: "You need to go to the school",
-      desc: "You need to go to market to get this job done2"
-    },
-    {
-      sno: 3,
-      title: "You need to go to the tuition",
-      desc: "You need to go to market to get this job done3"
-    },
-  ]);
+  const addTodo = (title, desc)=>{
+    console.log("I am adding this todo", title, desc)
+
+    let sno;
+    if(todos.length==0){
+      sno = 0;
+    } else {
+      sno = todos[todos.length-1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);  
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
   
   return (
     <>
       <Header title="My Todos List" searchBar={false}/>
-      <AddTodo/>
+      <AddTodo addTodo={addTodo}/>
       <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </>
